67d08312384016676a56a1d1ba5a7894f10609a5
nupponenveikko <nupponen.veikko@gmail.com>
2026-04-25 16:31:43 +0300

    v2

---
+++ app/src/main/java/com/example/htveikkonupponen/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.htveikkonupponen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonCreate, buttonList, buttonTraining, buttonBattle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonCreate = findViewById(R.id.buttonCreate);
+        buttonList = findViewById(R.id.buttonList);
+        buttonTraining = findViewById(R.id.buttonTraining);
+        buttonBattle = findViewById(R.id.buttonBattle);
+
+        buttonCreate.setOnClickListener(v ->
+                startActivity(new Intent(this, CreateLutemonActivity.class)));
+
+        buttonList.setOnClickListener(v ->
+                startActivity(new Intent(this, ListLutemonsActivity.class)));
+
+        buttonTraining.setOnClickListener(v ->
+                startActivity(new Intent(this, TrainingActivity.class)));
+
+        buttonBattle.setOnClickListener(v ->
+                startActivity(new Intent(this, BattleActivity.class)));
+    }
+}
